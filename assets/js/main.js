cb59bf1b7758d59fd5d5d237e0a47633962eaf08
Jahid Munshi <jahidmunshi99@gmail.com>
2023-08-07 01:41:15 +0600

    service section

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,9 @@ const dropDownToggle2 = document.querySelector("#dropdown2");
 const dropDownMenu = document.querySelector(".dorpdown-menu")
 const searchField = document.querySelector(".search-field");
 const searchToggle = document.querySelector("#search-smalldevice");
+const navbarStricky = document.querySelector(".navbar");
 
-
+// DropDown Toggle
 dropDownToggle1.addEventListener("click", ()=>{
     dropDownToggle1.classList.toggle("active");
 })
@@ -15,6 +16,16 @@ dropDownToggle2.addEventListener("click", ()=>{
     dropDownToggle2.classList.toggle("active");
 })
 
+// Top Search Bar Toggle
 searchToggle.addEventListener("click", ()=>{
     searchField.classList.toggle("active");
+})
+
+// Navbar Sticky
+window.addEventListener(scroll, ()=>{
+    if(this.scrollY>=100){
+        navbarStricky.classList.add('scrolled')
+    }else{
+        navbarStricky.classList.remove('scrolled')
+    }
 })
